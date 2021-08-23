8226fb6532500d4e7c99b185ab7211251e157260
Thyagaraj V <thyaguvenkat06@gmail.com>
2021-08-23 17:13:37 +0530

    search bar is added

--- js/order.js
+++ js/order.js
@@ -1,44 +1,34 @@
 loginCheck();
-let total=localStorage.getItem("TOTAL_BILL-AMOUNT");
-    document.querySelector("#totalAmount").value=total;
+let total = localStorage.getItem("TOTAL_BILL-AMOUNT");
+document.querySelector("#totalAmount").value = total;
 function orderNow() {
-
     const name = document.querySelector("#userName").value;
     const phonenumber = document.querySelector("#phoneNumber").value;
     const date = document.querySelector("#date").value;
     const address = document.querySelector("#address").value;
-     
-    let product =JSON.parse(localStorage.getItem("cartElements"));
-    
-    
+    let product = JSON.parse(localStorage.getItem("cartElements"));
     event.preventDefault();
-
-    // if (phonenumber.length != 10) {
-    //     alert("mobile number must be 10 digit");
-    // } else {
-        try{
-            OrderValidation.validate(name, phonenumber, date, address, product)
+    try {
+        OrderValidation.validate(name, phonenumber, date, address, product)
         let orderNow = {
             name: name,
             phonenumber: phonenumber,
             date: date,
             address: address,
-            productDetails:product
-
-        };  
-       productService.order(orderNow).then(res => {
+            productDetails: product
+        };
+        console.log(orderNow)
+        productService.order(orderNow).then(res => {
             alert("your order successfully placed");
+            
             window.location.href = "index.html";
         }).catch(err => {
             alert("order failed");
         });
-    }catch(err){
+    } catch (err) {
         console.log(err.message);
         alert(err.message);
     }
-    
-    }
-
-
+}
 const totalBillAmout = localStorage.getItem("TOTAL_BILL_AMOUNT");
-document.querySelector("#totalAmount").value=totalBillAmout;
+document.querySelector("#totalAmount").value = totalBillAmout;
--- js/product.js
+++ js/product.js
@@ -1,10 +1,10 @@
+const param = new URLSearchParams(window.location.search.substr(1));
+let category = (param.get("category"));
 
 function gettingData() {
 
   //getting category from url
-  const param = new URLSearchParams(window.location.search.substr(1));
-  var category = (param.get("category"));
-  var content = `<hr><h1 style=" margin-top: 20px;">Product for ${category}</h1><hr>`;
+  let content = `<hr><h1 style=" margin-top: 20px;">Product for ${category}</h1><hr>`;
   productService.products().then(res => {
     let data = res.data.rows;
     let petProducts = data.map(obj=>obj.doc);
@@ -12,9 +12,10 @@ function gettingData() {
       if (product.category == category) {
         console.log(product);
         content = content + `<div class="dogitems" id="dogitems">
-    <a href="viewitems.html?id=${product._id}"><img src="images/${product.imageUrl}" alt="image"></a><br>
+    <a href="viewitems.html?id=${product._id}">
+    <img src="images/${product.imageUrl}" alt="image"></a><br>
     <p>${product.productName}</p>
-    <p>${product.description}</p>
+   <p>${product.description}</p>
     <p>Price : ₹${product.price}</p>
     
   </div>`;
@@ -30,7 +31,42 @@ function gettingData() {
 }
 gettingData();
 
+function searchProducts(){
+  event.preventDefault();
+  console.log("Search Products");
+  //1. Get search text box value
+  let productName = document.querySelector("#search").value;
+  console.log("Search:", productName);
 
+  //2. GEt all products
+  productService.products().then(res=>{
+    const data=res.data.rows;
+    const productData=data.map(obj=>obj.doc);
+    console.log(productData);
+
+    //3. Filter products based on the given search key word
+    let filteredProducts = productData;
+    if(productName !=""){
+      filteredProducts = productData.filter(obj=>obj.category==category &&  obj.productName.toLowerCase().indexOf(productName.toLowerCase())!=-1);
+    }
+    console.table(filteredProducts);
+    let content="";
+    for(let product of filteredProducts){
+      
+        console.log(product);
+        content = content + `<div class="dogitems" id="dogitems">
+    <a href="viewitems.html?id=${product.productName}">
+    <img src="images/${product.imageUrl}" alt="image"></a><br>
+    <p>${product.productName}</p>
+   <p>${product.description}</p>
+    <p>Price : ₹${product.price}</p>
+    
+  </div>`;
+  document.querySelector("#dogContainer").innerHTML =  content;   
+    
+  }
+});
+}
 
 
 
